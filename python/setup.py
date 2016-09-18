648a4418f54976ebf508385e6d9cbaaed3d8e80e
Kevin Barnes <kbarnes3@gmail.com>
2016-09-18 10:55:33 -0700

    Adding setup file

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(name='fabricghdeploykeys',
+      version='1.0.0',
+      description='Helper library for Fabric to simplify creating and managing GitHub deploy keys when deploying GitHub-hosted repositories',
+      author='Kevin Barnes',
+      author_email='kbarnes3@gmail.com',
+      url='https://github.com/kbarnes3/FabricGHDeployKeys',
+      packages=['fabricghdeploykeys'])
