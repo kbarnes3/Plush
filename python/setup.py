67a6f3f1c8a0be501959980af60fb7ad1bf7dbd9
Kevin Barnes <kbarnes3@gmail.com>
2016-10-18 22:01:21 -0700

    Moving commands into submodule

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ setup(name='fabricghdeploykeys',
       author_email='kbarnes3@gmail.com',
       url='https://github.com/kbarnes3/FabricGHDeployKeys',
       install_requires=['oauth2client>=3.0.0', 'PyGithub>=1.28', 'keyring>=9.3.1'],
-      packages=['fabricghdeploykeys'],
+      packages=['fabricghdeploykeys', 'fabricghdeploykeys.fabric_commands'],
       entry_points={
           'console_scripts': [
               'auth = fabricghdeploykeys.console:auth_entry',
