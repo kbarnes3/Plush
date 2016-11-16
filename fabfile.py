eb217bb22d0643b33b88d8bae7272490e5c9ba1e
Kevin Barnes <kbarnes3@gmail.com>
2016-11-15 22:30:17 -0800

    Creating deployment SSH keys

--- fabfile.py
+++ fabfile.py
@@ -1,2 +1,8 @@
 from fabricghdeploykeys.fabric_commands import setup_user
-from fabricghdeploykeys.fabric_commands.ssh_key import make_key_dir
+from fabricghdeploykeys.fabric_commands.ssh_key import make_key_dir, create_key
+
+
+def test_deploy():
+    project_name = 'FabricGHDeployKeys'
+    owning_group = 'webadmin'
+    create_key(project_name, owning_group)
--- python/fabricghdeploykeys/fabric_commands/ssh_key.py
+++ python/fabricghdeploykeys/fabric_commands/ssh_key.py
@@ -1,8 +1,21 @@
-from fabric.api import sudo
+from fabric.api import run, sudo
 
 from .permissions import set_permissions
 
 
-def make_key_dir(owning_group, directory='/var/deploykeys'):
+DEFAULT_KEY_DIRECTORY = '/var/deploykeys'
+
+
+def make_key_dir(owning_group, directory=DEFAULT_KEY_DIRECTORY):
     sudo('mkdir -p {0}'.format(directory))
+    _set_key_dir_permissions(directory, owning_group)
+
+
+def create_key(project_name, owning_group, directory=DEFAULT_KEY_DIRECTORY):
+    keyfile = '{0}/{1}'.format(directory, project_name)
+    run('ssh-keygen -t rsa -b 4096 -C "{0}" -f {1}'.format(project_name, keyfile))
+    _set_key_dir_permissions(directory, owning_group)
+
+
+def _set_key_dir_permissions(directory, owning_group):
     set_permissions(directory, owning_group, '660', setgid=True)
