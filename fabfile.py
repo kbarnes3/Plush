e8560833c16a47ae0d88b546cececd0c20a002ad
Kevin Barnes <kbarnes3@gmail.com>
2017-05-19 18:11:01 -0700

    Replacing more references to FabricGHDeployKeys and new GitHub application

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,12 @@
-from fabricghdeploykeys.fabric_commands import setup_user
-from fabricghdeploykeys.repo_keys import add_repo_key
-from fabricghdeploykeys.fabric_commands.git import clone
-from fabricghdeploykeys.fabric_commands.permissions import make_directory
-from fabricghdeploykeys.fabric_commands.ssh_key import create_key
+from plush.fabric_commands import setup_user
+from plush.repo_keys import add_repo_key
+from plush.fabric_commands.git import clone
+from plush.fabric_commands.permissions import make_directory
+from plush.fabric_commands.ssh_key import create_key
 
 
 def test_deploy():
-    repo_full_name = 'kbarnes3/FabricGHDeployKeys'
+    repo_full_name = 'kbarnes3/Plush'
     owning_group = 'webadmin'
     create_key(repo_full_name, owning_group)
     add_repo_key(repo_full_name)
