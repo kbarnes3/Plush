66a12ed2eb1905f770e197c57866e4439f926fad
Kevin Barnes <kbarnes3@gmail.com>
2017-05-23 17:43:37 -0700

    Cleaning up the list of commands in fab -l

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,14 @@
-from plush.fabric_commands import setup_user
-from plush.repo_keys import add_repo_key
-from plush.fabric_commands.git import clone
-from plush.fabric_commands.permissions import make_directory
-from plush.fabric_commands.ssh_key import create_key
-
-
 def test_deploy():
+    from plush.fabric_commands import setup_user
+    from plush.repo_keys import add_repo_key
+    from plush.fabric_commands.git import clone
+    from plush.fabric_commands.permissions import make_directory
+    from plush.fabric_commands.ssh_key import create_key
+
     repo_full_name = 'kbarnes3/Plush'
     owning_group = 'webadmin'
     create_key(repo_full_name, owning_group)
     add_repo_key(repo_full_name)
     make_directory(owning_group, '/var/src')
     clone(repo_full_name, '/var/src/test')
+
