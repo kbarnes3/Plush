1f1fa803728455ef93e2d28622a151c1d1d88802
Kevin Barnes <kbarnes3@gmail.com>
2017-05-31 18:08:28 -0700

    setup.py sdist now runs

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+import setuptools
 
 long_description="""Plush is designed to SSH deploy keys on a remote computer using Fabric. These deploy keys are stored in the repo settings for your repo on GitHub using the GitHub API (using OAUTH credentials). Plush then clones your repo using a remote path that references these deploy keys. Plush allows multiple projects to have unique deploy keys all on one computer."""
 
-setup(name='plush-fabric',
+setuptools.setup(name='plush-fabric',
       version='0.1.0',
       description='Helper library for Fabric to simplify creating and managing GitHub deploy keys when deploying GitHub-hosted repositories',
       long_description=long_description,
