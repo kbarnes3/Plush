320a09bbdd4f599072b813bf6b9be7e2a64550d8
Kevin Barnes <kbarnes3@gmail.com>
2018-06-20 23:03:19 -0700

    Removing existing ssh key if needed

--- python/plush/fabric_commands/ssh_key.py
+++ python/plush/fabric_commands/ssh_key.py
@@ -1,4 +1,5 @@
 from fabric.api import run, sudo
+from fabric.contrib.files import exists
 
 from .permissions import make_directory, set_permissions_directory
 
@@ -16,5 +17,7 @@ def get_keyfile(project_name, directory=DEFAULT_KEY_DIRECTORY, public=True):
 def create_key(project_name, owning_group, directory=DEFAULT_KEY_DIRECTORY):
     make_directory(owning_group, directory, mod='600')
     keyfile = get_keyfile(project_name, directory, public=False)
+    if exists(keyfile, use_sudo=True):
+        sudo('rm {0}'.format(keyfile))
     sudo('ssh-keygen -t rsa -b 4096 -C "{0}" -f {1}'.format(project_name, keyfile))
     set_permissions_directory(directory, owning_group, mod='600')
