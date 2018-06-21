a869fdfd784d834c5cb773b216c5763f21406d9e
Kevin Barnes <kbarnes3@gmail.com>
2018-06-20 23:03:09 -0700

    Adding option to skip strict name checking

--- python/plush/fabric_commands/git.py
+++ python/plush/fabric_commands/git.py
@@ -2,6 +2,11 @@ from fabric.api import sudo
 from .ssh_key import get_keyfile
 
 
-def clone(repo_full_name, path):
+def clone(repo_full_name, path, skip_strict_key_checking=False):
     keyfile = get_keyfile(repo_full_name, public=False)
-    sudo("git clone 'ext::ssh -i {0} git@github.com %S {1}.git' {2}".format(keyfile, repo_full_name, path))
+    strict_key_checking = '-o StrictHostKeyChecking=no ' if skip_strict_key_checking else ''
+    sudo("git clone 'ext::ssh -i {0} {1}git@github.com %S {2}.git' {3}".format(
+        keyfile,
+        strict_key_checking,
+        repo_full_name,
+        path))
