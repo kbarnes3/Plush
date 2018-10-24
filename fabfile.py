96924b6221de7f33b72409200f5c220f86b3cf67
Kevin Barnes <kbarnes3@gmail.com>
2018-10-23 18:02:11 -0700

    Refactoring prepare_user

--- fabfile.py
+++ fabfile.py
@@ -5,16 +5,9 @@ env.use_ssh_config = True  # This makes it easier to use key based authenticatio
 
 def setup_user(user, no_sudo_passwd='', public_key_file=''):
     from fabric.api import sudo
-    from fabric.contrib.files import exists
-    from plush.fabric_commands import setup_user as setup_user_internal
+    from plush.fabric_commands import prepare_user
 
-    setup_user_internal(user, 'webadmin', add_sudo='yes')
-
-    if no_sudo_passwd:
-        sudoers_file = '/etc/sudoers.d/{0}-plush'.format(user)
-        if exists(sudoers_file, True):
-            sudo('rm {0}'.format(sudoers_file))
-        sudo("echo '{0} ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo -f {1}".format(user, sudoers_file))
+    prepare_user(user, 'webadmin', add_sudo=True, no_sudo_passwd=bool(no_sudo_passwd))
 
     if public_key_file:
         with open(public_key_file, 'r') as public_key:
--- python/plush/fabric_commands/__init__.py
+++ python/plush/fabric_commands/__init__.py
@@ -1,4 +1,5 @@
 from fabric.api import settings, sudo
+from fabric.contrib.files import exists
 from .ssh_key import get_keyfile
 
 
@@ -6,7 +7,8 @@ class _AllowedException(Exception):
     pass
 
 
-def setup_user(user, group, add_sudo='yes'):
+def prepare_user(user, group, add_sudo=True, no_sudo_passwd=False):
+    messages = ''
     user_exists = False
 
     with settings(abort_exception=_AllowedException):
@@ -17,7 +19,7 @@ def setup_user(user, group, add_sudo='yes'):
             pass
 
     if not user_exists:
-        sudo('adduser {0}'.format(user))
+        sudo('adduser --disabled-password {0}'.format(user))
 
     group_exists = False
     with settings(abort_exception=_AllowedException):
@@ -32,6 +34,18 @@ def setup_user(user, group, add_sudo='yes'):
 
     sudo('adduser {0} {1}'.format(user, group))
 
-    if add_sudo.lower() == 'yes':
+    if add_sudo:
         sudo('adduser {0} sudo'.format(user))
+
+    if no_sudo_passwd:
+        sudoers_file = '/etc/sudoers.d/{0}-plush'.format(user)
+        if exists(sudoers_file, True):
+            sudo('rm {0}'.format(sudoers_file))
+        sudo("echo '{0} ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo -f {1}".format(user, sudoers_file))
+
+    if add_sudo and not no_sudo_passwd:
+        messages += 'Ensure {0} has a secure password configured in order to run commands as sudo.\n'.format(user)
+    if not user_exists:
+        messages += 'The {0} account was created without a password set. A superuser will need to manually run ' \
+                    '\"sudo passwd {0}\"'.format(user)
 
