491b462f34df82b71bd4852d0218f98f664135d2
Kevin Barnes <kbarnes3@gmail.com>
2022-06-01 14:33:24 -0700

    Using a relative path on Windows

--- scripts/Console-Scripts.ps1
+++ scripts/Console-Scripts.ps1
@@ -8,6 +8,9 @@ Set-Item function:global:Update-DevEnvironment {
 Set-Item function:global:Upgrade-Requirements {
     Push-Location $PSScriptRoot\..
     & pip-compile -o .\win64-py310-requirements.txt .\requirements.in
+    $requirements = Get-Content .\win64-py310-requirements.txt
+    $requirements = $requirements -replace 'file:///.*Plush/python', './python'
+    Set-Content -Path .\win64-py310-requirements.txt -Value $requirements
     Pop-Location
     Write-Host 'win64-py310-requirements.txt updated.'
     Write-Host 'Run pip-sync win64-py310-requirements.txt to update your environment.'
