ed4eaace3dcc2df4ff91dedd2ef5c67cc557770b
Kevin Barnes <kbarnes3@gmail.com>
2022-06-01 13:51:55 -0700

    Generating requirements for Ubuntu

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,18 @@
+from colorama import init, Fore
 from fabric import Task
 from fabric.connection import Connection
+from fabric.transfer import Transfer
 from patchwork.files import exists as patchwork_exists
 
 import plush.fabric_commands
-from plush.fabric_commands import prepare_user
+from plush.fabric_commands import install_packages, prepare_user
 from plush.fabric_commands.git import clone
 from plush.fabric_commands.permissions import ensure_directory
 from plush.fabric_commands.ssh_key import create_key
 from plush.repo_keys import add_repo_key
+
+
+init(autoreset=True)
 
 
 def exists(conn: Connection, path: str) -> bool:
@@ -18,7 +23,8 @@ def exists(conn: Connection, path: str) -> bool:
 
 @Task
 def setup_user(conn, user, disable_sudo_passwd=False, set_public_key_file=None):
-    messages = plush.fabric_commands.prepare_user(
+    print(Fore.GREEN + f'Configuring {user}')
+    messages = prepare_user(
         conn,
         user,
         'webadmin',
@@ -31,6 +37,7 @@ def setup_user(conn, user, disable_sudo_passwd=False, set_public_key_file=None):
         print("========================================")
         print(messages)
         print("========================================")
+    print(Fore.GREEN + f'{user} configured')
 
 
 @Task
@@ -55,6 +62,7 @@ def disable_ssh_passwords(conn):
 
 @Task
 def test_deploy(conn, repo):
+    print(Fore.GREEN + 'Starting test deployment')
     owning_group = 'webadmin'
     create_key(conn, repo, owning_group)
     add_repo_key(conn, repo)
@@ -62,3 +70,56 @@ def test_deploy(conn, repo):
     if exists(conn, '/var/src/test'):
         conn.sudo('rm -rf /var/src/test')
     clone(conn, repo, '/var/src/test', skip_strict_key_checking=True)
+    print(Fore.GREEN + 'Test deployment complete to /var/src/test')
+
+
+@Task
+def compile_requirements(conn, fresh=False, upgrade=False):
+    print(Fore.GREEN + 'Compiling Python requirements')
+    install_packages(conn, ['python3-venv'])
+    remote_user = conn.run('whoami').stdout.strip()
+
+    staging_dir = '/tmp/pip-tools'
+    staging_python_dir = f'{staging_dir}/python'
+
+    ensure_directory(conn, staging_dir, remote_user)
+    conn.sudo(f'rm -rf {staging_dir}/*')
+    ensure_directory(conn, f'{staging_python_dir}', remote_user)
+    ensure_directory(conn, f'{staging_python_dir}/plush', remote_user)
+    ensure_directory(conn, f'{staging_python_dir}/plush/fabric_commands', remote_user)
+
+    requirements_in = 'test-requirements.in'
+    setup_py = 'setup.py'
+    readme_rst = 'README.rst'
+    requirements_txt = 'ubuntu64-py310-requirements.txt'
+
+    transfer = Transfer(conn)
+    transfer.put(requirements_in, f'{staging_dir}/{requirements_in}')
+    transfer.put(f'python/{setup_py}', f'{staging_python_dir}/{setup_py}')
+    transfer.put(f'python/{readme_rst}', f'{staging_python_dir}/{readme_rst}')
+
+    if not fresh:
+        transfer.put(requirements_txt, f'{staging_dir}/{requirements_txt}')
+
+    print(Fore.GREEN + 'Setting up virtualenv')
+    with conn.cd(staging_dir):
+        conn.run('python3 -m venv venv')
+
+        print(Fore.GREEN + 'Updating pip')
+        conn.run('venv/bin/python -m pip install --upgrade pip')
+
+        print(Fore.GREEN + 'Updating pip-tools')
+        conn.run('venv/bin/python -m pip install --upgrade pip-tools')
+
+        print(Fore.GREEN + 'Compiling requirements')
+        upgrade_flag = ''
+        if upgrade:
+            upgrade_flag = '--upgrade'
+        conn.run(f'venv/bin/pip-compile {upgrade_flag} --output-file={requirements_txt} ' +
+                 f'{requirements_in}')
+
+
+    transfer.get(f'{staging_dir}/{requirements_txt}', requirements_txt)
+    print(Fore.GREEN + f'Updated {requirements_txt}')
+    print(Fore.GREEN + 'Removing temp files')
+    conn.sudo('rm -rf /tmp/pip-tools')
