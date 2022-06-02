8510651792a956c90348615c9df8a54a43016026
Kevin Barnes <kbarnes3@gmail.com>
2022-06-01 19:24:42 -0700

    Adding scripts to publish

--- scripts/Console-Scripts.ps1
+++ scripts/Console-Scripts.ps1
@@ -1,5 +1,37 @@
 . $PSScriptRoot\Invoke-Fabric.ps1
 
+Set-Item function:global:Build-Plush {
+    Push-Location $PSScriptRoot\..\python
+    python -m build
+    Pop-Location
+}
+
+Set-Item function:global:Publish-Plush {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('TestPyPI','PyPI')]
+        [string]$Repository
+    )
+    Push-Location $PSScriptRoot\..\python
+    $version = Get-PlushVersion
+    if ($Repository -eq 'PyPI') {
+        python -m twine upload "dist/*$version*"
+    } else {
+        python -m twine upload --repository testpypi "dist/*$version*"
+    }
+    Pop-Location
+}
+
+Set-Item function:global:Get-PlushVersion {
+    Push-Location $PSScriptRoot\..
+    $regex = 'version = (.+)'
+    $matchingLines = (Get-Content .\python\setup.cfg) -match $regex
+    $matchingLines[0] -match $regex | Out-Null
+    $version = $Matches[1]
+    Pop-Location
+    return $version
+} -Force
+
 Set-Item function:global:Update-DevEnvironment {
     param([switch]$Verbose)
     . $PSScriptRoot\Update.ps1 -Verbose:$Verbose
